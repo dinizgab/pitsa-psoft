f52335c92bbac8fd6123f626cdae054afd1efdd2
Matheus Victor <matheusvictor.dev@gmail.com>
2023-05-08 02:15:46 -0300

    Renomeia o projeto para PitsA

--- src/main/java/com/ufcg/psoft/mercadofacil/repository/ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ufcg.psoft.mercadofacil.repository;
-
-import com.ufcg.psoft.mercadofacil.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
-
--- src/main/java/com/ufcg/psoft/mercadofacil/service/ProdutoCriarService.java
+++
@@ -1,9 +0,0 @@
-package com.ufcg.psoft.mercadofacil.service;
-
-import com.ufcg.psoft.mercadofacil.dto.ProdutoPostPutRequestDTO;
-import com.ufcg.psoft.mercadofacil.model.Produto;
-
-@FunctionalInterface
-public interface ProdutoCriarService {
-    Produto salvar(ProdutoPostPutRequestDTO produtoPostPutRequestDTO);
-}
