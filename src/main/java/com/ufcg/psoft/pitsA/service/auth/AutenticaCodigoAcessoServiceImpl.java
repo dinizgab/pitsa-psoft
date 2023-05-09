2ef51018d62374b5c06eb04d940074376fb03630
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2023-05-09 13:47:26 -0300

    refactor: making the auth service generic

--- src/main/java/com/ufcg/psoft/pitsA/service/auth/AutenticaCodigoAcessoServiceImpl.java
+++ src/main/java/com/ufcg/psoft/pitsA/service/auth/AutenticaCodigoAcessoServiceImpl.java
@@ -1,10 +1,10 @@
-package com.ufcg.psoft.pitsA.service.entregador;
+package com.ufcg.psoft.pitsA.service.auth;
 
 import com.ufcg.psoft.pitsA.exception.CodigoAcessoInvalidoException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AutenticaEmpregadoServiceImpl implements AutenticaEmpregadoService {
+public class AutenticaCodigoAcessoServiceImpl implements AutenticaCodigoAcessoService {
     @Override
     public void autenticar(String codigoAtual, String codigoAutenticar) throws CodigoAcessoInvalidoException {
         if (!codigoAtual.equals(codigoAutenticar)) {
--- src/main/java/com/ufcg/psoft/pitsA/service/entregador/AutenticaEmpregadoService.java
+++
@@ -1,6 +0,0 @@
-package com.ufcg.psoft.pitsA.service.entregador;
-
-@FunctionalInterface
-public interface AutenticaEmpregadoService {
-    void autenticar(String codigoAtual, String codigoAutenticar);
-}
