35cc2135cd33187874fab140421b0433ba396e4a
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2023-06-12 13:31:06 -0300

    feat: inicio dos testes

---
+++ src/test/java/com/ufcg/psoft/pitsA/service/entregador/EntregadorDispServiceTest.java
@@ -0,0 +1,10 @@
+package com.ufcg.psoft.pitsA.service.entregador;
+
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@DisplayName("Testes do service de disponibilidade d")
+public class EntregadorDispServiceTest {
+
+}
